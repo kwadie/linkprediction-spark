fb54d16e9387931f958a9fbb7f90ad32ef410eb6
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2015-01-30 12:10:47 +0100

    extracted a subgraph so it's easy to sample

--- src/main/scala/de/tuberlin/dima/aim3/linkpredection/Sampler.scala
+++ src/main/scala/de/tuberlin/dima/aim3/linkpredection/Sampler.scala
@@ -9,34 +9,43 @@ import org.apache.spark.rdd.RDD
 
 object Sampler {
 
+  val SEED = 0x3133F
+
   val POS = 'Y'
   val NEG = 'N'
-  
+
   def main(args: Array[String]) {
-    val inputPath = "c:/tmp/data/slashdot0902/Slashdot0902.txt" 
-    val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
+    val inputPath = "c:/tmp/data/slashdot0902/Slashdot0902.txt"
+    val conf = new SparkConf().setAppName("Simple Application").setMaster("local[*]")
     val sc = new SparkContext(conf)
-    
+
     val graph = GraphLoader.edgeListFile(sc, inputPath)
+    val selectedVertices = graph.vertices.sample(false, 0.20, SEED)
+    			.map { case (vid, atr) => vid.toInt }
+    			.take(25000)
+   
+    val verticesBroadcasted = sc.broadcast(selectedVertices.toSet)
 
- 
-    val edges1 = graph.edges.map(e => (e.dstId, e))
-    val edges2 = graph.edges.map(e => (e.srcId, e))
+    val subgraph = graph.subgraph(vpred = (id, attr) => verticesBroadcasted.value.contains(id.toInt) )
+    println(subgraph.vertices.count)
+    
+    val edges1 = subgraph.edges.map(e => (e.dstId, e))
+    val edges2 = subgraph.edges.map(e => (e.srcId, e))
     val hardNetagives = edges1.join(edges2)
-    			   .filter { case (_, (e1, e2)) => e1.srcId != e2.dstId }
-    			   .map { case (_, (e1, e2)) => (e1.srcId.toInt, e2.dstId.toInt, NEG) }
- 
-    val edgeIds = graph.edges.map { case Edge(v1, v2, w) => (v1.toInt, v2.toInt) }
+      .filter { case (_, (e1, e2)) => e1.srcId != e2.dstId }
+      .map { case (_, (e1, e2)) => (e1.srcId, e2.dstId, NEG) }
 
-    val vertices = graph.vertices.sample(false, 0.1, 0x1223).map(v => v._1.toInt)
-    val easyNegatives = vertices.cartesian(vertices) 
-    			//.subtract(edgeIds) 
-    			// takes too long, we can just assume that it's highly unlikely to get a POS here  
-    			.sample(false, 0.005, 0x313FF)
-    			.map { case (v1, v2) => (v1, v2, NEG) }
+    val edgeIds = subgraph.edges.map { case Edge(v1, v2, w) => (v1, v2) }
+
+    val vertices = subgraph.vertices.sample(false, 0.1, SEED).map(v => v._1)
+    val easyNegatives = vertices.cartesian(vertices)
+      //.subtract(edgeIds) 
+      // takes too long, we can just assume that it's highly unlikely to get a POS here  
+      .sample(false, 0.005, SEED)
+      .map { case (v1, v2) => (v1, v2, NEG) }
 
     hardNetagives.take(1000).union(easyNegatives.take(1000)).foreach(println(_))
-    
+
   }
 }
 
