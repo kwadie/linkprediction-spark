d1dba69e23df842cb3cbb6b8b0aa24a33e1ca8e8
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2015-02-06 22:15:46 +0100

    added classification code and some stuff to feature list object

---
+++ src/main/scala/de/tuberlin/dima/aim3/linkpredection/Classification.scala
@@ -0,0 +1,73 @@
+package de.tuberlin.dima.aim3.linkpredection
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+import org.apache.spark.mllib.classification.SVMWithSGD
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.classification.NaiveBayes
+import org.apache.spark.mllib.classification.LogisticRegressionWithSGD
+import org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS
+import org.apache.spark.mllib.tree.RandomForest
+import org.apache.spark.mllib.tree.configuration.BoostingStrategy
+import org.apache.spark.mllib.tree.GradientBoostedTrees
+import org.apache.spark.ml.tuning.CrossValidator
+
+object Classification {
+
+  val SEED = 0x1312
+  val numIterations = 100
+
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("Simple Application").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+
+    val featuresPath = "C:/tmp/spark/out/features.txt"
+    val features = sc.textFile(featuresPath, 4)
+      .map(FeatureList.fromCsv)
+      .map(_.toLabeledPoint)
+
+    val splits = features.randomSplit(Array(0.6, 0.4), seed = SEED)
+
+    val training = splits(0).cache()
+    val test = splits(1)
+
+    //    val model = SVMWithSGD.train(training, numIterations)
+    //    val model = NaiveBayes.train(training)
+    //    val model = LogisticRegressionWithSGD.train(training, numIterations)
+
+    //    val numClasses = 2
+    //	val categoricalFeaturesInfo = Map[Int, Int]()
+    //	val numTrees = 3 // Use more in practice.
+    //	val featureSubsetStrategy = "auto" // Let the algorithm choose.
+    //	val impurity = "gini"
+    //	val maxDepth = 4
+    //	val maxBins = 32
+    //    
+    //    val model = RandomForest.trainClassifier(training, 2, categoricalFeaturesInfo, numTrees, 
+    //    							featureSubsetStrategy, impurity, maxDepth, maxBins, SEED)
+
+    val boostingStrategy = BoostingStrategy.defaultParams("Classification")
+    boostingStrategy.numIterations = 10
+    boostingStrategy.treeStrategy.numClasses = 2
+    boostingStrategy.treeStrategy.maxDepth = 5
+    boostingStrategy.treeStrategy.categoricalFeaturesInfo = Map[Int, Int]()
+
+    val model = GradientBoostedTrees.train(training, boostingStrategy)
+
+    val scoreAndLabels = test.map { point =>
+      val score = model.predict(point.features)
+      (score, point.label)
+    }
+
+    val metrics = new BinaryClassificationMetrics(scoreAndLabels)
+    val auROC = metrics.areaUnderROC()
+
+    println(f"AUC: $auROC")
+
+    // save ROC curve to file for visualization
+    // metrics.roc.toArray
+  }
+
+}
--- src/main/scala/de/tuberlin/dima/aim3/linkpredection/FeatureList.scala
+++ src/main/scala/de/tuberlin/dima/aim3/linkpredection/FeatureList.scala
@@ -3,6 +3,9 @@ package de.tuberlin.dima.aim3.linkpredection
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder
 import org.apache.commons.lang3.builder.ToStringStyle
 import org.apache.commons.lang3.ObjectUtils
+import org.apache.spark.util.Vector
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
 
 @SerialVersionUID(114L)
 class FeatureList(xv: Long, xu: Long, xlabel: String) extends Serializable with Comparable[FeatureList] {
@@ -18,10 +21,18 @@ class FeatureList(xv: Long, xu: Long, xlabel: String) extends Serializable with 
 
   override def toString = ReflectionToStringBuilder.toString(this, ToStringStyle.SHORT_PREFIX_STYLE)
 
-  def asCsv = f"$v,$u,$v_degree,$u_degree,$commonFriends_Out,$totalFriends_out,$getPrefAttachment,$label"
+  def toFeaturesArray = Array(v, u, v_degree, u_degree, commonFriends_Out, totalFriends_out, getPrefAttachment)
 
+  def toCsv = toFeaturesArray.mkString(",") + "," + label
+
+  def toVector = Vectors.dense(toFeaturesArray.map(_.toDouble))
+  
+  def toLabeledPoint = LabeledPoint(numericalLabel, toVector)
+  
   def getPrefAttachment: Int = v_degree * u_degree
 
+  def numericalLabel = if (label == "Y") 1.0 else 0.0
+  
   def setUdegree(ud: Int): FeatureList = {
     u_degree = ud
     return this
@@ -56,4 +67,26 @@ class FeatureList(xv: Long, xu: Long, xlabel: String) extends Serializable with 
 
   override def hashCode() = Seq(u, v).hashCode
 
+}
+
+object FeatureList {
+    def fromCsv(csv: String): FeatureList = {
+    val split = csv.split(",")
+    val it = split.iterator
+  
+    val u = it.next.toLong
+    val v = it.next.toLong
+    val v_degree = it.next.toInt
+    val u_degree = it.next.toInt
+    val commonFriends_Out = it.next.toInt
+    val totalFriends_out = it.next.toInt
+    val prefAttachment = it.next.toInt
+    val label = it.next
+
+    new FeatureList(u, v, label)
+    				.setUdegree(u_degree)
+    				.setVdegree(v_degree)
+    				.setCommonFriendsOut(commonFriends_Out)
+    				.setTotalFriendsOut(totalFriends_out)
+  } 
 }
