601d14d93e3721029cb27ebfe6bfb674545edac2
Karim Wadie <karim.wadie@gmail.com>
2015-02-07 00:31:40 +0100

    added all features

--- src/main/scala/de/tuberlin/dima/aim3/linkpredection/FeatureList.scala
+++ src/main/scala/de/tuberlin/dima/aim3/linkpredection/FeatureList.scala
@@ -5,25 +5,69 @@ import org.apache.commons.lang3.builder.ToStringStyle
 import org.apache.commons.lang3.ObjectUtils
 
 @SerialVersionUID(114L)
-class FeatureList(xv: Long, xu: Long, xlabel: String) extends Serializable with Comparable[FeatureList] {
+class FeatureList(xu: Long, xv: Long, xlabel: String) extends Serializable with Comparable[FeatureList] {
 
-  var v: Long = xv
   var u: Long = xu
-  var v_degree: Int = 0
+  var v: Long = xv
+  
   var u_degree: Int = 0
+  var v_degree: Int = 0
+  
+  var u_in: Int =0
+  var v_in:Int =0
+  
+  var u_out:Int = 0
+  var v_out: Int = 0
+  
+  var u_bi: Int =0
+  var v_bi: Int =0
+  
+  var commonFriends_In: Int = 0
   var commonFriends_Out: Int = 0
+  
+  var totalFriends_in: Int = 0
   var totalFriends_out: Int = 0
-  var prefAttachment: Int = v_degree * u_degree
+  
+  var friendsMeasure:Int =0;
+  
   var label: String = xlabel
-
+  
   override def toString = ReflectionToStringBuilder.toString(this, ToStringStyle.SHORT_PREFIX_STYLE)
+  
+  def  headerCsv = "u,v,u_dg,u_in,u_out,u_bi,u_indensity,u_outdensity,u_bidensity,"+
+                  "v_dg,v_in,v_out,v_bi,v_indensity,v_outdensity,v_bidensity,"+
+                  "cf_in,cf_out,tf_in,tf_out,fm,pa,jcin,jcout,label"
+  
 
-  def asCsv = f"$v,$u,$v_degree,$u_degree,$commonFriends_Out,$totalFriends_out,$getPrefAttachment,$label"
+  def asCsv = 
+              f"$u,$v,$u_degree,$u_in,$u_out,$u_bi,$calcUinDensity,$calcUoutDensity,$calcUbiDensity,"+
+              f"$v_degree,$v_in,$v_out,$v_bi,$calcVinDensity,$calcVoutDensity,$calcVbiDensity,"+
+              f"$commonFriends_In,$commonFriends_Out,$totalFriends_in,$totalFriends_out,$friendsMeasure,"+
+              f"$calcPrefAttachment,$calcJaccardIn,$calcJaccardOut,$label"
 
-  def getPrefAttachment: Int = v_degree * u_degree
+  //calculated features
+  def calcPrefAttachment: Int = v_degree * u_degree
+  def calcJaccardOut:Double =  if ( totalFriends_out != 0) commonFriends_Out.toDouble/totalFriends_out.toDouble else 0.0D 
+  def calcJaccardIn:Double =  if ( totalFriends_in != 0) commonFriends_In.toDouble/totalFriends_in.toDouble else 0.0D 
+  def calcVinDensity:Double =   if(v_degree != 0)   v_in.toDouble / v_degree.toDouble  else 0.0D
+  def calcVoutDensity:Double =   if(v_degree != 0)   v_out.toDouble  / v_degree.toDouble  else 0.0D
+  def calcVbiDensity:Double =   if(v_degree != 0)   v_bi.toDouble  / v_degree.toDouble  else 0.0D
+  def calcUinDensity:Double =   if(u_degree != 0)   u_in.toDouble  / u_degree.toDouble  else 0.0D
+  def calcUoutDensity:Double =   if(u_degree != 0)   u_out.toDouble  / u_degree.toDouble  else 0.0D
+  def calcUbiDensity:Double =   if(u_degree != 0)   u_bi.toDouble  / u_degree.toDouble  else 0.0D
 
   def setUdegree(ud: Int): FeatureList = {
     u_degree = ud
+    return this
+  }
+  
+     def setUindegree(id: Int): FeatureList = {
+    u_in = id
+    return this
+  }
+   
+    def setUoutdegree(id: Int): FeatureList = {
+    u_out = id
     return this
   }
 
@@ -31,14 +75,51 @@ class FeatureList(xv: Long, xu: Long, xlabel: String) extends Serializable with 
     v_degree = vd
     return this
   }
-
+  
+   def setVindegree(id: Int): FeatureList = {
+    v_in = id
+    return this
+  }
+   
+    def setVoutdegree(id: Int): FeatureList = {
+    v_out = id
+    return this
+  }
+    
+  def setVbidegree(d: Int): FeatureList = {
+    v_bi = d
+    return this
+  }
+  
+    def setUbidegree(d: Int): FeatureList = {
+    u_bi = d
+    return this
+  }
+    
+    
+    
   def setCommonFriendsOut(cf: Int): FeatureList = {
     commonFriends_Out = cf
+    return this
+  }
+  
+    def setCommonFriendsIn(cf: Int): FeatureList = {
+    commonFriends_In = cf
     return this
   }
 
   def setTotalFriendsOut(tf: Int): FeatureList = {
     totalFriends_out = tf
+    return this
+  }
+  
+  def setTotalFriendsIn(tf: Int): FeatureList = {
+    totalFriends_in = tf
+    return this
+  }
+  
+   def setFriendsMeasure(fm: Int): FeatureList = {
+    friendsMeasure = fm
     return this
   }
 
@@ -56,4 +137,15 @@ class FeatureList(xv: Long, xu: Long, xlabel: String) extends Serializable with 
 
   override def hashCode() = Seq(u, v).hashCode
 
+}
+
+
+class VertexDegrees(d:Int, in:Int, out:Int){
+  
+  var degree:Int = d
+  var indegree:Int = in
+  var outdegree:Int = out
+  
+  override def toString = ReflectionToStringBuilder.toString(this, ToStringStyle.SHORT_PREFIX_STYLE)
+  
 }
--- src/main/scala/de/tuberlin/dima/aim3/linkpredection/FeaturesCalc.scala
+++ src/main/scala/de/tuberlin/dima/aim3/linkpredection/FeaturesCalc.scala
@@ -5,8 +5,10 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import org.apache.spark._
 import org.apache.spark.graphx._
-// To make some of the examples work we will also need RDD
 import org.apache.spark.rdd.RDD
+import org.apache.spark.rdd.PairRDDFunctions
+import org.apache.spark.rdd.PairRDDFunctions
+import org.apache.spark.rdd.PairRDDFunctions
 
 
 /**
@@ -17,36 +19,79 @@ object FeaturesCalc {
   
   def main(args: Array[String]) {
     
+    
+    
     //setting the environment
-    val inputPath = "D:\\datasets\\Small Graph\\edges.txt" //"D:\\datasets\\slashdot0902\\Slashdot0902.txt" 
+   // val inputPath = "D:\\datasets\\Small Graph\\edges.txt" 
+  //  val inputPath = "D:\\datasets\\slashdot0902\\Slashdot0902.txt" 
+    val inputPath = "D:\\datasets\\IMPRO3DEBUG\\inputgraph\\10nodes\\edges10.txt"
+    
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
     val sc = new SparkContext(conf)
+    
    
     //spark Util to construct a graph from adjacency list 
     val graph = GraphLoader.edgeListFile(sc, inputPath)
     
-    //get in+out degrees of nodes
+    //get bi,in,out degrees of nodes to be used in Join
+    //NOTE: only vertices with values exist in the RDD - do left join or SolSet++
     val degreesRDD:RDD[(Long,Int)] = graph.degrees.map(x=> (x._1,x._2))
+    val indegreesRDD:RDD[(Long,Int)] = graph.inDegrees.map(x=> (x._1,x._2))
+    val outdegreesRDD:RDD[(Long,Int)] = graph.outDegrees.map(x=> (x._1,x._2))
     
+
+    val alldegreesRDD = degreesRDD.leftOuterJoin(indegreesRDD)
+                                  .mapValues(f=> new VertexDegrees( f._1, f._2.getOrElse(0), f._1 - f._2.getOrElse(0)   ) )
+                        
+   
+        
+     println("original graph edges: "+ graph.edges.count())
     
+     
    
     //FIXME: Sample easy and hard negative too
     //construct a sample of input data (positive links) for dev, Return an RDD[Edge[Int]
     //Then map each edge to a FeatureList object
-    var solSet:RDD[FeatureList] = graph.edges//.sample(false, 1)
-                                   .map { edge => new FeatureList(edge.srcId,edge.dstId)}
+    var solSet:RDD[FeatureList] = graph.edges//.sample(false, 0.01)
+                                   .map { edge => new FeatureList(edge.srcId,edge.dstId,"Y")}
     
+    
+    
+    println("original sol set: "+solSet.count())
     
     //create an RDD with v as key and current feature list as value and join it with degrees to comput v degree
     var solSetRDD_V:RDD[(Long,FeatureList)] = solSet.map { x => (x.v,x) }
-    solSet = solSetRDD_V.join(degreesRDD).map(f => f._2._1.setVdegree(  f._2._2  ) )
+    
+    //get v degrees
+    solSet = solSetRDD_V.join(alldegreesRDD)
+                        .map{f => 
+                             f._2._1.setVdegree( f._2._2.degree )
+                                    .setVindegree(f._2._2.indegree)
+                                    .setVoutdegree(f._2._2.outdegree)
+                             }
+                                         
+   
     
     //do the same with u
-    val solSetRDD_U:RDD[(Long,FeatureList)] = solSet.map { x => (x.u,x) }
-    solSet = solSetRDD_U.join(degreesRDD).map(f => f._2._1.setUdegree(  f._2._2  ) )
+    var solSetRDD_U:RDD[(Long,FeatureList)] = solSet.map { x => (x.u,x) }
+    
+    //get u degrees
+      solSet = solSetRDD_U.join(alldegreesRDD)
+                          .map{f => 
+                               f._2._1.setUdegree( f._2._2.degree )
+                                      .setUindegree(f._2._2.indegree)
+                                      .setUoutdegree(f._2._2.outdegree)
+                             }
+  
     
     //prepare and RDD of edges with edg.src as key (to be used in joins)
     val edgesRDD:RDD[(Long,Long)] = graph.edges.map { x => (x.srcId,x.dstId) }
+      
+    val reverseEdgesRDD:RDD[(Long,Long)] = graph.edges.reverse.map { x => (x.srcId,x.dstId) }
+      
+    
+     
+    
     
     /*
      *  Total friends: distinct number of outgoing neigbours for v and u combined
@@ -54,22 +99,122 @@ object FeaturesCalc {
      *  then map the result to get  (FeatureList, DestinationID) =  for each group key (FeatureList) with a neigbour node of either u or v from the original entry
      *  then group by key (FeatureList) (or v,u pair) and get the distinct number of neigbour nodes, this is the total frirend measure 
      */
-    solSet = solSet.flatMap { f => Seq(  (f.v,f) , (f.u,f) ) }.join(edgesRDD).map( x=>  (x._2._1, x._2._2)   ).groupByKey().mapValues (_.toSet.size).map(f=> f._1.setTotalFriendsOut(f._2)  )
+    
+    //temp hold operations result to be merged into the solSet
+    var opResult = solSet.flatMap { f => Seq(  (f.v,f) , (f.u,f) ) }
+                   .join(edgesRDD)
+                   .map( x=>  (x._2._1, x._2._2)   ).groupByKey()
+                   .mapValues(_.toSet.size)
+                   .map(f=> f._1.setTotalFriendsOut(f._2)  )
+                   
+    solSet = opResult ++ solSet.subtract(opResult)   
+    
+    //totalfriends_in
+    opResult = solSet.flatMap { f => Seq(  (f.v,f) , (f.u,f) ) }
+                   .join(reverseEdgesRDD)
+                   .map( x=>  (x._2._1, x._2._2)   ).groupByKey()
+                   .mapValues(_.toSet.size)
+                   .map(f=> f._1.setTotalFriendsIn(f._2)  )
+                   
+    solSet = opResult ++ solSet.subtract(opResult)   
+                   
+    
     
 
     /*
      * Same as total friends but ommit ((FL,DestinatinID), 1) then reduce by key (FL,Des)
      * Then change the key to FL only and count how many destinationIDs appeared more than once in each group (FL) , these are the common friends
      */
-    solSet  = solSet.flatMap { f => Seq(  (f.v,f) , (f.u,f) ) }.join(edgesRDD).map( f=>  (f._2,1) ).reduceByKey((x,y)=> x+y).map(x=> (x._1._1, x._2 )).groupByKey().mapValues(x =>  x.filter ( _ > 1 ).size ).map( f=> f._1.setCommonFriendsOut(f._2))
+    opResult  = solSet.flatMap { f => Seq(  (f.v,f) , (f.u,f) ) }
+                    .join(edgesRDD)
+                    .map( f=>  (f._2,1) )
+                    .reduceByKey((x,y)=> x+y)
+                    .map(x=> (x._1._1, x._2 )).groupByKey()
+                    .mapValues(x =>  x.filter ( _ > 1 ).size )
+                    .map( f=> f._1.setCommonFriendsOut(f._2))
     
+    solSet = opResult ++ solSet.subtract(opResult)  
     
-    solSet.foreach(println(_))
+    //common_in
+    opResult  = solSet.flatMap { f => Seq(  (f.v,f) , (f.u,f) ) }
+                    .join(reverseEdgesRDD)
+                    .map( f=>  (f._2,1) )
+                    .reduceByKey((x,y)=> x+y)
+                    .map(x=> (x._1._1, x._2 )).groupByKey()
+                    .mapValues(x =>  x.filter ( _ > 1 ).size )
+                    .map( f=> f._1.setCommonFriendsIn(f._2))
     
+    solSet = opResult ++ solSet.subtract(opResult)  
+    
+  
     
    
-
+   
+   
+   /*
+    * calculate FriendsMeasure
+    * The number of connections between v and u friends.
+    * The number of edges between the totalfriendsList and itself
+    * 
+    * Some of the 1st degree neigbours might not have outgoing edges and that will lead to losing the inital featurelist 
+    * Add the diff between the sets and the result to construct the updated SolSet
+    */ 
+        opResult =  solSet.flatMap { f => Seq(  (f.v,f) , (f.u,f) ) }
+                    .join(edgesRDD)
+                    .map( _._2.swap )
+                    .join(edgesRDD)
+                    .map( x=> (x._2._1,  (x._2._2, x._1)  )  )
+                    .groupByKey()
+                    .mapValues{ x=> 
+                      val firstNB = x.map(_._1).toSet
+                      val secondNB = x.map(_._2).toSet
+                      secondNB.count(x => firstNB.contains(x))
+                    }.map(f => f._1.setFriendsMeasure(f._2) )
+                    
+    solSet = opResult ++ solSet.subtract(opResult)       
     
+    
+                   
+   //v bidegree
+    opResult =  solSet.map ( f => (f.v,f) )
+                     .join(edgesRDD)
+                     .map(x=> (x._2._2,( x._1, x._2._1 )) )
+                     .join(edgesRDD)
+                     .map( x=> (x._2._1, x._2._2 ) ).groupByKey()
+                     .map( x =>  x._1._2.setVbidegree(  x._2.count(p=> p == x._1._1) ) )
+                     
+     solSet = opResult ++ solSet.subtract(opResult)   
+
+                    
+   
+    
+    //u bidegree
+    opResult =  solSet.map ( f => (f.u,f) )
+                     .join(edgesRDD)
+                     .map(x=> (x._2._2,( x._1, x._2._1 )) )
+                     .join(edgesRDD)
+                     .map( x=> (x._2._1, x._2._2 ) ).groupByKey()
+                     .map( x =>  x._1._2.setUbidegree(  x._2.count(p=> p == x._1._1) ) )
+                     
+     solSet = opResult ++ solSet.subtract(opResult)   
+        
+        
+     
+     //RDD.saveAsTextFile fails, so collect the RDD and print on local machine
+     //print to csv file
+     val outpath = "D:\\tmp\\scalalinkpred\\featurecalc\\features.csv"
+     new java.io.File(outpath).delete()
+     
+     val solSetCSV = solSet.map { _.asCsv }.collect()
+     
+     val pw = new java.io.PrintWriter(new java.io.File( outpath ))
+         pw.println(new FeatureList(0,0,"").headerCsv + "")
+         solSetCSV.foreach {pw.println(_)}
+         pw.close
+         
+         println("----------- FINISHED -------------")
+     
+
   }
   
 
